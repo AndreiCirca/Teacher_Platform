91553cdd7687f688735e344bbb5cfd23a82af867
lauramuresan <lauramuresan420@gmail.com>
2026-05-18 12:29:25 +0300

    Initial Springboot setup

---
+++ src/main/java/com/example/TeacherPlatform/TeacherPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeacherPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeacherPlatform/TeacherPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeacherPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
